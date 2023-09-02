5c060cc50a13b341be8e4ece73036eaa87840725
tahmid <tahmidrahman2812@outlook.com>
2023-09-02 18:59:55 +0100

    feat: progress updates and change 21M/M21 to male 21

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from utils.CustomStream import CustomStream
 import sys, re, datetime
 
 # Specify the file name where you want to save the output
-current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-output_file = f"log-{current_time}.txt"
+# current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+output_file = f"log.txt"
 
 # Create or open the output file for writing
 with open(output_file, "w") as file:
@@ -18,7 +18,8 @@ with open(output_file, "w") as file:
     headers = connect_reddit()
     posts_df = get_hot_posts(headers)
 
-    for index, row in posts_df.iloc[22:].iterrows():
+    for index, row in posts_df.iloc[24:].iterrows():
+        print(f"Progress: {index} / {posts_df.shape[0]}")
         title = row[1]
         post_text = row[2]
 
@@ -27,6 +28,8 @@ with open(output_file, "w") as file:
         post_text = post_text.replace("AITA", "Am I the asshole")
         post_text = post_text.replace("(", ",")
         post_text = post_text.replace(")", ",")
+        post_text = re.sub(r'(\d+)([mfMF])', lambda match: ', ' + match.group(1) + (' Male,' if match.group(2).lower() == 'm' else ' Female,'), post_text)
+        post_text = re.sub(r'([mfMF])(\d+)', lambda match: (', Female ' if match.group(1).lower() == 'f' else ', Male ') + match.group(2) + ",", post_text)
         
         generate_speech(syn, title + "\n" + post_text, re.sub(r'[^a-zA-Z0-9\s]', '', title))
 
